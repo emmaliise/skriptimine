8d5368ed681e0ea189929a4e7e83748819284aa5
emmaliise <emma-liise.peterson@voco.ee>
2026-09-04 12:39:24 +0300

    Lisatud praks3

---
+++ praks3/tervitus.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Skript küsib kasutajalt ees- ja perenime ning tervitab teda.
+
+echo -n "Sisesta oma ees- ja perenimi: "
+read eesnimi perenimi
+
+echo "Tere tulemast, $eesnimi $perenimi!"
